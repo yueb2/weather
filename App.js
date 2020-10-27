8251f1c765be22df80af076c5ce727a466a7d9cc
yueb2 <yueb2@naver.com>
2020-10-27 16:49:35 +0900

    How can change styles.

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.yelloView}>
+        <Text>Hello</Text>
+      </View>
+      <View style={styles.blueView}>
+        <Text>Hello</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +19,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  yelloView: {
+    flex: 0.5,
+    backgroundColor: "yellow",
+  },
+  blueView: {
+    flex: 2,
+    backgroundColor: "blue",
   },
 });

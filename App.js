8d19289592df40b8426b76787fb83175ec1a127a
yueb2 <yueb2@naver.com>
2020-10-28 16:39:23 +0900

    make a loading page

--- App.js
+++ App.js
@@ -1,31 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yelloView}>
-        <Text>Hello</Text>
-      </View>
-      <View style={styles.blueView}>
-        <Text>Hello</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  yelloView: {
-    flex: 0.5,
-    backgroundColor: "yellow",
-  },
-  blueView: {
-    flex: 2,
-    backgroundColor: "blue",
-  },
-});
